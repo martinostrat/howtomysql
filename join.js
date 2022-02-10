d7eeb3bf9af568ca2ed851c82be0b5b0e3e00b60
unknown <martin.ostrat@khk.ee>
2022-02-10 10:51:40 +0200

    join

---
+++ join.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'qwerty',
+    database: 'mydb'
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log('Connected');
+    let sql = "select users.name as user, products.name as favourite from users join products on users.fav_product = products.id";
+
+    con.query(sql, function(err, result, fields) {
+        if(err) throw err;
+        console.log(result);
+    });
+});
